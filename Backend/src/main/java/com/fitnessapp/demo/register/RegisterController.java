d7162eea25c9c0f8aeb95958f68332232ee7dd80
Lucas <l.pont@student.han.nl>
2025-11-10 22:28:36 +0100

    too tired i sleep
    need to implement the DAO methods to put data in the database, also need to finish the controller

--- Backend/src/main/java/com/fitnessapp/demo/register/RegisterController.java
+++ Backend/src/main/java/com/fitnessapp/demo/register/RegisterController.java
@@ -3,11 +3,17 @@ package com.fitnessapp.demo.register;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class RegisterController {
 
     @GetMapping("/register")
     public String registerUser(){
+        ArrayList<RegisterDAO> list = new ArrayList<>();
+        list.registerUser();
+
+
         return "registered";
     }
 }
--- Backend/src/main/java/com/fitnessapp/demo/register/RegisterDTO.java
+++ Backend/src/main/java/com/fitnessapp/demo/register/RegisterDTO.java
@@ -1,4 +1,31 @@
 package com.fitnessapp.demo.register;
 
 public class RegisterDTO {
+    private String username;
+    private String password;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
